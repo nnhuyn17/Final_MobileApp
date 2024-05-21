4ebf07627c26f121a1b07a34bc97808ff8a3055d
ngochuyen <ngochuyen2k17@gmail.com>
2024-05-22 02:27:55 +0700

    done

--- frontend/lib/pages/admin/homeBlogadmin.dart
+++ frontend/lib/pages/admin/homeBlogadmin.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-
+import 'package:frontend/widgets/contact_section.dart';
+import 'package:frontend/widgets/projects_section.dart';
 import '../../constants/colors.dart';
+import '../../constants/size.dart';
+import '../../widgets/drawer_mobile.dart';
+import '../../widgets/footer.dart';
+import '../../widgets/header_desktop.dart';
+import '../../widgets/main_desktop.dart';
+import '../../widgets/main_mobile.dart';
+import '../../widgets/skill_desktop.dart';
+import '../../widgets/skills_mobile.dart';
+
 
 class HomeBlogAd extends StatefulWidget {
   const HomeBlogAd({Key? key}) : super(key: key);
@@ -14,227 +20,123 @@ class HomeBlogAd extends StatefulWidget {
 }
 
 class _HomeBlogAdState extends State<HomeBlogAd> {
-  late DateTime _selectedDate = DateTime.now();
-  String _timeRange = '9am-11am';
-  String _content = '';
-  late String accountId = ''; // Initialize with an empty string
-  var pathURLL = "https://backend-final-web.onrender.com";
-
-  final _storage = FlutterSecureStorage();
-
-  @override
-  void initState() {
-    super.initState();
-    _getAccountId();
-  }
-
-  Future<void> _getAccountId() async {
-    accountId = await _storage.read(key: "accountId") ?? '';
-    // Update the state to rebuild the widget
-    setState(() {});
-  }
-
-
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: _selectedDate,
-      firstDate: DateTime.now(),
-      lastDate: DateTime(2101),
-    );
-    if (picked != null && picked != _selectedDate)
-      setState(() {
-        _selectedDate = picked;
-      });
-  }
-
-  void _handleTimeChange(String? selectedTime) {
-    if (selectedTime != null) {
-      setState(() {
-        _timeRange = selectedTime;
-      });
-    }
-  }
-
-  void _handleContentChange(String enteredContent) {
-    setState(() {
-      _content = enteredContent;
-    });
-  }
-
-  void _handleSubmit() async {
-    final String fullDate = '${_selectedDate.toIso8601String().split('T')[0]}';
-    final bool? isConfirmed = await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Confirmation'),
-          content: Text('Are you sure you want to submit?'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context, true);
-              },
-              child: Text('Yes'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context, false);
-              },
-              child: Text('No'),
-            ),
-          ],
-        );
-      },
-    );
-
-    if (isConfirmed != null && isConfirmed) {
-      try {
-        final response = await http.post(
-          Uri.parse(
-              '$pathURLL/createMeeting'), // Replace with your actual endpoint
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: jsonEncode({
-            'user_id': accountId,
-            'date': fullDate,
-            'time_range': _timeRange,
-            'content': _content,
-            'status': 'pending',
-          }),
-        );
-
-        if (response.statusCode == 201) {
-          Fluttertoast.showToast(msg: 'Success');
-        } else {
-          print('Error response body: ${response.body}');
-          print('Error response status code: ${response.statusCode}');
-          Fluttertoast.showToast(msg: 'Error');
-        }
-      } catch (error) {
-        print('Error: $error');
-        Fluttertoast.showToast(msg: 'An error occurred');
-      }
-    }
-  }
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final scrollController = ScrollController();
+  final List<GlobalKey> navbarKeys = List.generate(4, (index) => GlobalKey());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('PixelPulse Coder_Blog'),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(20.0),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.grey,
-              width: 1.0,
-            ),
-            borderRadius: BorderRadius.circular(8.0),
+    final screenSize = MediaQuery.of(context).size;
+    final screenWidth = screenSize.width;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          key: scaffoldKey,
+          endDrawer: constraints.maxWidth >= kMinDesktopWidth
+              ? null
+              : DrawerMobile(
+            onNavItemTap: (int navIndex) {
+              // call function
+              scaffoldKey.currentState?.closeEndDrawer();
+              scrollToSection(navIndex);
+            },
           ),
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Date Meeting'),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextButton(
-                      onPressed: () => _selectDate(context),
-                      child: Text(
-                        "${_selectedDate.toLocal()}".split(' ')[0],
-                        style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text('Time'),
-              DropdownButton<String>(
-                value: _timeRange,
-                onChanged: _handleTimeChange,
-                items: <String>[
-                  '9am-11am',
-                  '1pm-3pm',
-                  '3pm-5pm',
-                  '5pm-7pm',
-                  '7pm-9pm',
-                ].map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              SizedBox(height: 20.0),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1.0,
-                  ),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: TextField(
-                    maxLines: 10,
-                    onChanged: _handleContentChange,
-                    decoration: InputDecoration(
-                      hintText: 'Enter your message...',
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20.0),
-              SizedBox(
-                width: double.maxFinite,
-                child: ElevatedButton(
-                  onPressed: _handleSubmit,
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        CustomColor.bluePrimary),
-                    foregroundColor: MaterialStateProperty.all<Color>(
-                        Colors.white),
-                  ),
-                  child: Text('Submit'),
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Center(
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    maxWidth: 300,
-                  ),
-                  child: const Divider(),
-                ),
-              ),
-              SizedBox(height: 15),
-              Center(
-                child: Wrap(
-                  spacing: 12,
-                  runSpacing: 12,
-                  alignment: WrapAlignment.center,
-                  children: [
-                    Image.asset("assets/images/github.png", width: 28),
-                    Image.asset("assets/images/linkedin.png", width: 28),
-                    Image.asset("assets/images/facebook.png", width: 28),
-                    Image.asset("assets/images/instagram.png", width: 28),
-                    Image.asset("assets/images/telegram.png", width: 28),
-                  ],
-                ),
+          appBar: constraints.maxWidth >= kMinDesktopWidth
+              ? null
+              : AppBar(
+            title: const Text('PixelPulse Coder'),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {
+                  scaffoldKey.currentState?.openEndDrawer();
+                },
               ),
             ],
           ),
-        ),
-      ),
+          body: SingleChildScrollView(
+            controller: scrollController,
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                SizedBox(key: navbarKeys.first),
+                // Header
+                if (constraints.maxWidth >= kMinDesktopWidth)
+                  HeaderDesktop(
+                    onNavMenuTap: (int navIndex) {
+                      // call function
+                      scrollToSection(navIndex);
+                    },
+                  ),
+                // else
+                //   HeaderMobile(
+                //     onLogoTap: () {},
+                //     onMenuTap: () {
+                //       scaffoldKey.currentState?.openEndDrawer();
+                //     },
+                //   ),
+                // Main content
+                if (constraints.maxWidth >= kMinDesktopWidth)
+                  const MainDesktop()
+                else
+                  const MainMobile(),
+                const Divider(), // Divider between main content and skills
+                // SKILLS
+                Container(
+                  key: navbarKeys[1],
+                  width: screenWidth,
+                  padding: const EdgeInsets.fromLTRB(25, 20, 25, 60),
+                  color: Colors.white,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        "What I can do ?",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: CustomColor.textFieldBg,
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                      if (constraints.maxWidth >= kMedDesktopWidth)
+                        const SkillsDesktop()
+                      else
+                        const SkillsMobile(),
+                    ],
+                  ),
+                ),
+                const Divider(), // Divider between skills and projects
+                const SizedBox(height: 30),
+                // PROJECTS
+                ProjectSection(key: navbarKeys[2]),
+                const Divider(), // Divider between projects and contact
+                const SizedBox(height: 30),
+                // CONTACT
+                ContactSection(key: navbarKeys[3]),
+                const SizedBox(height: 30),
+                // FOOTER
+                const Footer(),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void scrollToSection(int navIndex) {
+    if (navIndex == 4) {
+      // open a blog page
+      // js.context.callMethod('open', [SnsLinks.blog]);
+      return;
+    }
+
+    final key = navbarKeys[navIndex];
+    Scrollable.ensureVisible(
+      key.currentContext!,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
     );
   }
 }
